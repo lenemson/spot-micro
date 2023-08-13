03f25dfff68fbc207677e66a8547f68fe9b4f705
Jeremy Ibanez <jeremy.ibanez@protonmail.com>
2023-08-13 21:46:17 +0200

    Initial commit

---
+++ main/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include "esp_log.h"
+
+static const char* TAG = "spot-micro";
+
+void app_main(void)
+{
+    ESP_LOGI(TAG, "Starting application");
+}
