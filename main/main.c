72ff0a767ad4b85490670e27990a84bf0694ef87
Jeremy Ibanez <jeremy.ibanez@protonmail.com>
2023-08-14 17:05:55 +0200

    Handle relay with GPIO 27

--- main/main.c
+++ main/main.c
@@ -2,10 +2,25 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 #include "esp_log.h"
+#include "driver/gpio.h"
 
-static const char* TAG = "spot-micro";
+#define RELAY_GPIO GPIO_NUM_27
+#define PCA9685_I2C_ADDRESS 0x40
+
+static const char *TAG = "spot-micro";
 
 void app_main(void)
 {
     ESP_LOGI(TAG, "Starting application");
+
+    gpio_set_direction(RELAY_GPIO, GPIO_MODE_OUTPUT);
+    gpio_set_level(RELAY_GPIO, 1); // 1 = on, 0 = off
+
+    while (true)
+    {
+        gpio_set_level(RELAY_GPIO, 0);
+        vTaskDelay(1000);
+        gpio_set_level(RELAY_GPIO, 1);
+        vTaskDelay(1000);
+    }
 }
